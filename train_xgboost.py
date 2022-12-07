3e7968298030e2370cd36d02e858358e3b907419
Vivian <vivian.vanoijen@surf.nl>
2022-12-07 20:18:52 +0100

    Add script to train xgb from command line

---
+++ train_xgboost.py
@@ -0,0 +1,45 @@
+import argparse
+import json
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import xgboost as xgb
+import matplotlib.pyplot as plt
+
+from xgboost_function import CopyrightXGBoost
+
+
+def main(args: argparse.Namespace):
+    xgboost = CopyrightXGBoost()
+    xgboost.train_xgboost(Path(args.data), Path(args.out), args.train)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data",
+        type=str,
+        default="data/traindata.csv",
+        help="input csv file that contains the data",
+    )
+    parser.add_argument(
+        "--out",
+        type=str,
+        default="/out",
+        help="name of the folder that output will be saved to",
+    )
+    parser.add_argument(
+        "--train",
+        type=float,
+        default=0.8,
+        help="percentage of data used for training (as opposed to evaluation)",
+    )
+    args = parser.parse_args()
+    main(args)
